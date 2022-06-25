5ac84a4ad265a207d85369603ed6956ea82eec16
Richard Rodriguez-Van Dusen <richardRVD88@gmail.com>
2022-06-25 16:36:52 -0500

    FizzBuzz Solved

---
+++ Week1/Day4/FizzBuzz/FizzBuzz.js
@@ -0,0 +1,14 @@
+var x = 1
+
+for(var x = 1; x<101; x+=1){
+    if(x % 3 == 0){
+        console.log("Fizz")
+    }
+    else if(x % 5 == 0){
+        console.log("Buzz")
+    }
+    else if(x % 3&&5 == 0){
+        console.log("FizzBuzz")
+    }
+    else(console.log (x))
+}
