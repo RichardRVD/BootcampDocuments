8b0590ec6aafbd72242d138dcc8f1d2b26a2c740
Richard Rodriguez-Van Dusen <richardRVD88@gmail.com>
2022-06-28 13:38:37 -0500

    LikesPart2Completed

---
+++ Week2/Day2/Likes/Part2/script.js
@@ -0,0 +1,15 @@
+function addLike(){
+    var like = document.getElementById('likes1').innerText
+    like++;
+    document.getElementById('likes1').innerText = like;
+}
+function addLike2(){
+    var like = document.getElementById('likes2').innerText
+    like++;
+    document.getElementById('likes2').innerText = like;
+}
+function addLike3(){
+    var like = document.getElementById('likes3').innerText
+    like++;
+    document.getElementById('likes3').innerText = like;
+}
