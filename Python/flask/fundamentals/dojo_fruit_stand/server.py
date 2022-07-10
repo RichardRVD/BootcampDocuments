936c8f16cfb3aa79c56018ac8439ad3247f9d449
Richard Rodriguez-Van Dusen <richardRVD88@gmail.com>
2022-07-10 14:38:30 -0500

    dojo_fruit_standCompleted

--- Python/flask/fundamentals/dojo_fruit_stand/server.py
+++ Python/flask/fundamentals/dojo_fruit_stand/server.py
@@ -7,13 +7,21 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    number = value
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    s_id = request.form['student_id']
+    full_name = first_name +''+ last_name
+    s_count = request.form['strawberry']
+    r_count = request.form['raspberry']
+    a_count = request.form['apple']
+    number = int(s_count) + int(r_count) + int(a_count)
+    print(f"Charging {full_name} for {number} fruits.")
     print(request.form)
-    return render_template("checkout.html", number)
+    return render_template("checkout.html", number = number, first_name = first_name, last_name = last_name, full_name = full_name, s_id = s_id, s_count = s_count, a_count = a_count, r_count = r_count)
 
 @app.route('/fruits')         
 def fruits():
     return render_template("fruits.html")
 
 if __name__=="__main__":   
-    app.run(debug=True)    
+    app.run(debug=True, port=5001)    
